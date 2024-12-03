70f4e140bd1e3267e99ef094a9c236d6c228a699
duwutape <raven.langheld@gmx.de>
2024-12-04 00:30:48 +0100

    day 1

--- src/day01/Day01.java
+++ src/day01/Day01.java
@@ -2,8 +2,6 @@ package day01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Day01 {
@@ -16,6 +14,32 @@ public class Day01 {
     }
 
     private void solve() throws FileNotFoundException {
+        File file = new File("src/day01/input.txt");
+        Scanner scanner = new Scanner(file);
 
+        int floor = 0;
+        int pos = 0;
+        boolean count = true;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            for (int i = 0; i < line.length(); i++) {
+                if(count){
+                    pos++;
+                }
+                if (line.charAt(i) == '(') {
+                    floor += 1;
+                } else {
+                    floor -= 1;
+                }
+                if(count && floor == -1){
+                    count = false;
+                }
+            }
+
+        }
+        System.out.println(floor);
+        System.out.println(pos);
     }
 }

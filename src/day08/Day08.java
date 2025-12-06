3009bc0c08bfa61f2a7d3e63e2e78acfa56b89f7
duwutape <quinn.langheld@gmx.de>
2025-12-06 23:43:45 +0100

    feat(day8): solved day 8

--- src/day08/Day08.java
+++ src/day08/Day08.java
@@ -20,5 +20,38 @@ public class Day08 {
     private void solve() throws FileNotFoundException {
         File input = new File("src/day08/input.txt");
         Scanner scanner = new Scanner(input);
+
+        int charsPt1 = 0;
+        int memoryPt1 = 0;
+        int charsPt2 = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String linePt1 = line;
+
+            charsPt1 += linePt1.length();
+
+            linePt1 = linePt1.replaceAll("\\\\{2}", "!");
+            linePt1 = linePt1.replaceAll("\\\\\"", "?");
+            linePt1 = linePt1.replaceAll("\\\\x..", "§");
+            linePt1 = linePt1.replaceAll("\"", "");
+
+            memoryPt1 += linePt1.length();
+
+
+            String linePt2 = line;
+            linePt2 = linePt2.replaceAll("\\\\","!!");
+            linePt2 = linePt2.replaceAll("\"","??");
+            charsPt2 += linePt2.length()+2;
+
+        }
+
+        int memoryPt2 = charsPt1;
+
+        int res1 = charsPt1 - memoryPt1;
+        int res2= charsPt2 - memoryPt2;
+
+        System.out.println("res pt. 1: " + res1);
+        System.out.println("res pt. 2: " + res2);
     }
 }
